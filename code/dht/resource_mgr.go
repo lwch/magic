5c9bce2a5192f0f8c264c8bd9106eb18f6eaa759
lwch <lwch748@gmail.com>
2021-02-02 10:39:43 +0800

    修改握手包

--- code/dht/resource_mgr.go
+++ code/dht/resource_mgr.go
@@ -82,17 +82,13 @@ func (mgr *resMgr) loopGet() {
 	}
 }
 
-var handshakePrefix = []byte{
-	19, 66, 105, 116, 84, 111, 114, 114, 101, 110, 116, 32, 112, 114,
-	111, 116, 111, 99, 111, 108, 0, 0, 0, 0, 0, 16, 0, 1,
-}
-
 // http://www.bittorrent.org/beps/bep_0003.html
 func makeHandshake(hash hashType) []byte {
 	ret := make([]byte, 68)
 	ret[0] = 19
 	copy(ret[1:], protocol)
 	ret[24] = 0x10 // http://www.bittorrent.org/beps/bep_0010.html
+	ret[26] = 1
 	copy(ret[28:], hash[:])
 	id := data.RandID()
 	copy(ret[48:], id[:])
