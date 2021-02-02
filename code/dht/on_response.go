10fa87975d1a62553318b241276dbbdcf53a8630
lwch <lwch748@gmail.com>
2021-02-02 18:03:46 +0800

    修改log

--- code/dht/on_response.go
+++ code/dht/on_response.go
@@ -3,6 +3,7 @@ package dht
 import (
 	"bytes"
 	"encoding/binary"
+	"encoding/hex"
 	"net"
 	"strings"
 	"time"
@@ -77,7 +78,7 @@ func (n *node) onGetPeersResp(buf []byte, hash hashType) {
 	var found data.GetPeersResponse
 	err = bencode.Decode(buf, &found)
 	if err != nil {
-		logging.Error("decode get_peers response(found) failed" + n.errInfo(err))
+		logging.Error("decode get_peers response(found) failed: %s"+n.errInfo(err), hex.Dump(buf))
 		return
 	}
 	// n.dht.tk.add(found.Response.Token, hash, n.id)
