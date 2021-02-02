52be60ac5d905b912d1b86dce9a89952dc42d075
lwch <lwch748@gmail.com>
2021-02-02 17:14:28 +0800

    修改log

--- code/dht/resource_mgr.go
+++ code/dht/resource_mgr.go
@@ -299,8 +299,8 @@ func (mgr *resMgr) get(r resReq) {
 				return
 			}
 			if len(files.Name) > 0 {
-				logging.Info("recv: hash=%s, name=%s, length=%d",
-					r.id.String(), files.Name, files.Length)
+				logging.Info("recv: hash=%s, name=%s, length=%d, meta_length=%d",
+					r.id.String(), files.Name, files.Length, metaSize)
 			}
 			for _, file := range files.Files {
 				logging.Info("recv: hash=%s, path=%v, length=%d",
