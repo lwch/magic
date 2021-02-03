7f0207702e56108d2aeb599c9a918dfde9269cfb
lwch <lwch748@gmail.com>
2021-02-03 14:33:05 +0800

    修正test case

--- code/dht/table_test.go
+++ code/dht/table_test.go
@@ -15,11 +15,16 @@ func init() {
 }
 
 func TestTable(t *testing.T) {
-	table := newTable(nil, 8)
+	cfg := NewConfig()
+	dht, err := New(cfg)
+	if err != nil {
+		t.Fatal(err)
+	}
+	table := newTable(dht, 8)
 	var id hashType
 	for i := 0; i < 10000; i++ {
 		rand.Read(id[:])
-		table.add(newNode(nil, id, net.UDPAddr{}))
+		table.add(newNode(dht, id, net.UDPAddr{}))
 	}
 	bk, height, cnt := printTable(table.root, "")
 	fmt.Printf("max: %s %d %d\n", bk.String(), height, cnt)
@@ -39,10 +44,10 @@ func printTable(bk *bucket, dir string) (hashType, int, int) {
 		return prefix1, bits1, cnt1 + cnt2
 	}
 	var ids []string
-	for _, node := range bk.nodes {
-		ids = append(ids, node.id.String())
+	for n := bk.nodes.Front(); n != nil; n = n.Next() {
+		ids = append(ids, n.Value.(*node).id.String())
 	}
 	fmt.Printf("%s: %s %d %s\n",
 		bk.prefix.String(), dir, bk.bits, strings.Join(ids, ","))
-	return bk.prefix, bk.bits, len(bk.nodes)
+	return bk.prefix, bk.bits, bk.nodes.Len()
 }
