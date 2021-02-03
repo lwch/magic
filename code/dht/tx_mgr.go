8fbb240a6257d7a328dd0833eb4e572dc512a57d
lwch <lwch748@gmail.com>
2021-02-03 15:46:51 +0800

    修改find函数

--- code/dht/tx_mgr.go
+++ code/dht/tx_mgr.go
@@ -2,13 +2,15 @@ package dht
 
 import (
 	"container/list"
+	"encoding/binary"
 	"sync"
 
 	"github.com/lwch/magic/code/data"
 )
 
 type tx struct {
-	id     string       // transaction id
+	id     string // transaction id
+	idHash uint32
 	hash   hashType     // get_peers.info_hash
 	remote hashType     // find_node.target
 	t      data.ReqType // request type
@@ -34,6 +36,18 @@ func (mgr *txMgr) size() int {
 	return mgr.list.Len()
 }
 
+func txHash(tx string) uint32 {
+	if len(tx) > 4 {
+		bt := []byte(tx)
+		var hash uint32
+		for i := 0; i < len(bt)/4; i++ {
+			hash += binary.BigEndian.Uint32(bt[i*4:])
+		}
+		return hash
+	}
+	return 0xcccccccc
+}
+
 func (mgr *txMgr) add(id string, t data.ReqType, hash hashType, remote hashType) {
 	if mgr.size() >= mgr.max {
 		mgr.Lock()
@@ -43,6 +57,7 @@ func (mgr *txMgr) add(id string, t data.ReqType, hash hashType, remote hashType)
 	mgr.Lock()
 	mgr.list.PushBack(tx{
 		id:     id,
+		idHash: txHash(id),
 		hash:   hash,
 		remote: remote,
 		t:      t,
@@ -51,10 +66,12 @@ func (mgr *txMgr) add(id string, t data.ReqType, hash hashType, remote hashType)
 }
 
 func (mgr *txMgr) find(id string) *tx {
+	idHash := txHash(id)
 	var node *list.Element
 	mgr.RLock()
 	for node = mgr.list.Back(); node != nil; node = node.Prev() {
-		if node.Value.(tx).id == id {
+		if node.Value.(tx).idHash == idHash &&
+			node.Value.(tx).id == id {
 			break
 		}
 	}
