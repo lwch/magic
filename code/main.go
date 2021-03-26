d462b7b12a886250039f1a36def12da24d1dd80d
lwch <lwch748@gmail.com>
2021-03-26 15:25:07 +0800

    去除过滤

--- code/main.go
+++ code/main.go
@@ -11,7 +11,6 @@ import (
 	"github.com/lwch/magic/code/dht"
 	"github.com/lwch/magic/code/logging"
 	"github.com/lwch/runtime"
-	"github.com/oschwald/geoip2-golang"
 )
 
 var bootstrapAddrs []*net.UDPAddr
@@ -33,99 +32,10 @@ func init() {
 }
 
 func main() {
-	geo, err := geoip2.Open("GeoLite2-Country.mmdb")
-	runtime.Assert(err)
 	cfg := dht.NewConfig()
 	cfg.MinNodes = 100000
-	var names = []string{
-		"AG", // Ares
-		"A~", // Ares
-		"AR", // Arctic
-		"AV", // Avicora
-		"AX", // BitPump
-		"AZ", // Azureus
-		"BB", // BitBuddy
-		"BC", // BitComet
-		"BF", // Bitflu
-		"BG", // BTG (uses Rasterbar libtorrent)
-		"BR", // BitRocket
-		"BS", // BTSlave
-		"BX", // ~Bittorrent X
-		"CD", // Enhanced CTorrent
-		"CT", // CTorrent
-		"DE", // DelugeTorrent
-		"DP", // Propagate Data Client
-		"EB", // EBit
-		"ES", // electric sheep
-		"FT", // FoxTorrent
-		"FW", // FrostWire
-		"FX", // Freebox BitTorrent
-		"GS", // GSTorrent
-		"HL", // Halite
-		"HN", // Hydranode
-		"KG", // KGet
-		"KT", // KTorrent
-		"LH", // LH-ABC
-		"LP", // Lphant
-		"LT", // libtorrent
-		"lt", // libTorrent
-		"LW", // LimeWire
-		"MO", // MonoTorrent
-		"MP", // MooPolice
-		"MR", // Miro
-		"MT", // MoonlightTorrent
-		"NX", // Net Transport
-		"PD", // Pando
-		"qB", // qBittorrent
-		"QD", // QQDownload
-		"QT", // Qt 4 Torrent example
-		"RT", // Retriever
-		"S~", // Shareaza alpha/beta
-		"SB", // ~Swiftbit
-		"SS", // SwarmScope
-		"ST", // SymTorrent
-		"st", // sharktorrent
-		"SZ", // Shareaza
-		"TN", // TorrentDotNET
-		"TR", // Transmission
-		"TS", // Torrentstorm
-		"TT", // TuoTu
-		"UL", // uLeecher!
-		"UT", // µTorrent
-		"UW", // µTorrent Web
-		"VG", // Vagaa
-		"WD", // WebTorrent Desktop
-		"WT", // BitLet
-		"WW", // WebTorrent
-		"WY", // FireTorrent
-		"XL", // Xunlei
-		"XT", // XanTorrent
-		"XX", // Xtorrent
-		"ZT", // ZipTorrent
-	}
-	idxName := 0
-	maxNames := len(names)
-	cfg.GenID = func() [20]byte {
-		n := idxName % maxNames
-		idxName++
-
-		var id [20]byte
-		id[0] = '-'
-		id[1] = names[n][0]
-		id[2] = names[n][1]
-		rand.Read(id[3:])
-		for i := 3; i < 7; i++ {
-			id[i] = '0' + id[i]%10
-		}
-		id[7] = '-'
-		return id
-	}
 	cfg.NodeFilter = func(ip net.IP, id [20]byte) bool {
-		country, err := geo.Country(ip)
-		if err != nil {
-			return true
-		}
-		return country.Continent.Names["zh-CN"] == "亚洲"
+		return false
 	}
 	mgr, err := dht.New(cfg)
 	runtime.Assert(err)
